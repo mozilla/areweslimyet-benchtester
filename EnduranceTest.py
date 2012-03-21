53d3c143a0e0fb9a7fa7bb734897dd8fbe8457dc
John Schoenick <jschoenick@mozilla.com>
2012-03-21 14:37:30 -0700

    [EnduranceTest] Move up the deletion of the profile directory so it's deleted when we bail out
    Also remove a bunch of trailing spaces

--- EnduranceTest.py
+++ EnduranceTest.py
@@ -20,25 +20,25 @@ class EnduranceTest(BenchTester.BenchTest):
     parent.add_argument('--jsbridge_port', help="Port to use for jsbridge, so concurrent tests don't collide", default="24242")
     self.name = "EnduranceTest"
     self.parent = parent
-  
+
   def setup(self):
     self.info("Setting up Endurance module")
     self.ready = True
     self.endurance_results = None
-    
+
     if 'jsbridge_port' in self.parent.args:
       self.jsport = int(self.parent.args['jsbridge_port'])
     else:
       self.jsport = 24242
     return True
-  
+
   def endurance_event(self, obj):
     if obj['iterations']:
       self.info("Got enduranceResults callback")
       self.endurance_results = obj
     else:
       self.error("Got endurance test result with 0 iterations: %s" % obj)
-  
+
   def endurance_checkpoint(self, obj):
     if obj['checkpoints']:
       self.info("Got enduranceCheckpoint callback")
@@ -47,17 +47,17 @@ class EnduranceTest(BenchTester.BenchTest):
       self.endurance_results['iterations'].append(obj)
     else:
       self.error("Got endurance checkpoint with no data: %s" % obj)
-      
+
   def run_test(self, testname, testvars={}):
     if not self.ready:
       return self.error("run_test() called before setup")
-    
+
     self.info("Beginning endurance test '%s'" % testname)
-    
+
     import mozmill
     import mozrunner
     import jsbridge
-    
+
     profdir = tempfile.mkdtemp("slimtest_profile")
     self.info("Using temporary profile %s" % profdir)
     #
@@ -71,7 +71,7 @@ class EnduranceTest(BenchTester.BenchTest):
     # to avoid keeping everything in the runtime (it records a lot of numbers,
     # which in turn inflate memory usage, which it's trying to measure)
     mozmillinst.add_listener(self.endurance_checkpoint, eventType='mozmill.enduranceCheckpoint')
-    
+
     profile = mozrunner.FirefoxProfile(binary=self.tester.binary,
                                        profile=profdir,
                                        addons=[jsbridge.extension_path, mozmill.extension_path],
@@ -94,13 +94,13 @@ class EnduranceTest(BenchTester.BenchTest):
 
     runner = runnerwrap(binary=self.tester.binary, profile=profile)
     runner.cmdargs += ['-jsbridge', str(self.jsport)]
-    
+
     # Add test
     testpath = os.path.join(*testvars['test'])
     if not os.path.exists(testpath):
       return self.error("Test '%s' specifies a test that doesn't exist: %s" % (testname, testpath))
     mozmillinst.tests = [ testpath ]
-    
+
     # Run test
     self.endurance_results = None
     self.info("Endurance - starting browser")
@@ -115,20 +115,19 @@ class EnduranceTest(BenchTester.BenchTest):
         shutil.rmtree(profdir)
       except: pass
       return self.error("Endurance test run failed -- %s: %s" % (type(e), e))
-    
+
     self.info("Endurance - cleaning up")
     try:
       mozmillinst.stop()
+      shutil.rmtree(profdir)
     except Exception, e:
       self.error("Failed to properly cleanup mozmill -- %s: %s" % (type(e), e))
-    
-    shutil.rmtree(profdir)
-      
+
     self.info("Endurance - saving results")
-    
+
     if not self.endurance_results:
       return self.error("Test did not return any endurance data!")
-      
+
     results = {}
     for x in range(len(self.endurance_results['iterations'])):
       iteration = self.endurance_results['iterations'][x]
@@ -144,7 +143,7 @@ class EnduranceTest(BenchTester.BenchTest):
         label = label_re.group(1)
         for memtype,memval in checkpoint['memory'].items():
           results["/".join(["Iteration %u" % iternum, label, memtype])] = memval
-    
+
     if not self.tester.add_test_results(testname, results, successful):
       return self.error("Failed to save test results")
     if not successful:
